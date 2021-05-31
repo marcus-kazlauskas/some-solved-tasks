74fac711a91c2b92d300d38201cd80d36cac8035
Mikhail Kozlov <kozlov.mihael@yandex.ru>
2021-05-31 12:43:49 +0300

    new task added

--- src/Task7.scala
+++ src/Task7.scala
@@ -6,17 +6,16 @@ object Task7 {
    * Задача про рекурсивное дерево (взято с HackerRank)
    */
   def getLine(n: Int, m: Int, i: Int): String = {
-    if (n > 0)
-      if (i < m)
-        ("_" * (m * 2)) + "1" + ("_" * (m * 2 - 1))
-      else if (i < m * 2)
-        ("_" * (m * 3 - (i + 1))) + "1" + ("_" * ((i - m) * 2 + 1)) + "1" + ("_" * (m * 3 - (i + 2)))
-      else if (m > 1)
-        getLine(n - 1, m / 2, i - (m * 2)) * 2
-      else
-        getLine(n - 1, 1, 0)
-    else
+    if (n == 0)
       "_" * (m * 4)
+    else if (i < m)
+      ("_" * (m * 2)) + "1" + ("_" * (m * 2 - 1))
+    else if (i < m * 2)
+      ("_" * (m * 3 - (i + 1))) + "1" + ("_" * ((i - m) * 2 + 1)) + "1" + ("_" * (m * 3 - (i + 2)))
+    else if (m > 1)
+      getLine(n - 1, m / 2, i - (m * 2)) * 2
+    else
+      getLine(n - 1, 1, 0)
   }
 
   @tailrec
